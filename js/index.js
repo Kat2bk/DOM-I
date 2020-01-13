2da044fbe90ad34c0697ede806bd218af2f3ca09
unknown <Kat2bk@gmail.com>
2020-01-13 15:46:31 -0800

    fixed navigation, images, and button

--- js/index.js
+++ js/index.js
@@ -36,7 +36,61 @@ const siteContent = {
     "copyright" : "Copyright Great Idea! 2018"
   },
 };
-
 // Example: Update the img src for the logo
+// IMAGES
 let logo = document.getElementById("logo-img");
-logo.setAttribute('src', siteContent["nav"]["img-src"])
+logo.setAttribute('src', siteContent["nav"]["img-src"]);
+let img2 = document.getElementById("cta-img");
+img2.setAttribute('src', siteContent["cta"]["img-src"]);
+let img3 = document.getElementById("middle-img");
+img3.setAttribute('src', siteContent["main-content"]["middle-img-src"]);
+
+// anchor tags
+
+// let anchors = document.querySelectorAll("header nav a")[0];
+// anchors.textContent = (siteContent["nav"]["nav-item-1"])
+
+// let anchors1 = document.querySelectorAll("header nav a")[1];
+// anchors1.textContent = (siteContent["nav"]["nav-item-2"])
+
+let anchors = document.querySelectorAll("a");
+anchors.forEach((item, index) => item.textContent = siteContent['nav'][`nav-item-${index += 1}`])
+
+let title = document.querySelectorAll(".cta-text h1")[0];
+title.textContent = (siteContent["cta"]["h1"]);
+
+let titleButton = document.querySelectorAll(".cta-text button")[0];
+titleButton.textContent = (siteContent["cta"]["button"]);
+
+// features 
+let featureTitle = document.querySelectorAll(".main-content .top-content .text-content h4")[0];
+featureTitle.textContent = (siteContent["main-content"]["features-h4"]);
+
+let featurePara = document.querySelectorAll(".text-content p")[1];
+featurePara.textContent = (siteContent["main-content"]["features-content"]);
+
+// about
+
+let aboutTitle = document.querySelectorAll(".main-content .top-content .text-content h4")[2];
+aboutTitle.textContent = (siteContent["main-content"]["about-h4"]);
+
+let aboutPara = document.querySelectorAll(".top-content .text-content p")[3];
+aboutPara.textContent = (siteContent["main-content"]["about-content"]);
+
+// services
+
+let mainFeatures3 = document.querySelectorAll(".main-content .bottom-content .text-content h4")[4];
+mainFeatures3.textContent = (siteContent["main-content"]["services-h4"]);
+
+let mainParagraph3 = document.querySelectorAll(".main-content .bottom-content .text-content p")[2];
+mainParagraph3.textContent = (siteContent["main-content"]["services-content"]);
+
+// Product
+
+let mainFeatures4 = document.querySelectorAll(".main-content .bottom-content .text-content h4")[3];
+mainFeatures4.textContent = (siteContent["main-content"]["product-h4"]);
+
+let mainParagraph4 = document.querySelectorAll(".main-content .bottom-content .text-content p")[3];
+mainParagraph4.textContent = (siteContent["main-content"]["product-content"]);
+
+
